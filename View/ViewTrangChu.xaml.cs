e08fb6e7bcb0858dcc54a0a826bef35d3dc23815
An Tuan Vu-67th2-0197867 <0197867@st.huce.edu.vn>
2025-05-30 01:07:12 +0700

    antuanvu
    upgiaodienfn

---
+++ View/ViewTrangChu.xaml.cs
@@ -0,0 +1,49 @@
+﻿using DALTUDTXDHONG.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DALTUDTXDHONG.View
+{
+    /// <summary>
+    /// Interaction logic for ViewTrangChu.xaml
+    /// </summary>
+    public partial class ViewTrangChu : Window
+    {
+        public ViewTrangChu()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVatLieu_Click(object sender, RoutedEventArgs e)
+        {
+            frame_Body.Content = new pageVatLieu();
+        }
+
+        private void btnTietDien_Click(object sender, RoutedEventArgs e)
+        {
+            frame_Body.Content = new pageTietDien();
+        }
+
+        private void btnTinhToanKNCNLechTam_Click(object sender, RoutedEventArgs e)
+        {
+            frame_Body.Content = new pageTinhToan();
+        }
+
+        private void btnThongSoCot_Click(object sender, RoutedEventArgs e)
+        {
+            frame_Body.Content = new pageColumnParameters();
+
+        }
+    }
+}
